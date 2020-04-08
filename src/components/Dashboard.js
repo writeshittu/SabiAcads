4e40d651cabde48402d15e56569e90c29f383528
shit2boy <writeshittu@gmail.com>
2020-04-08 18:47:14 +0100

    .

---
+++ src/components/Dashboard.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+class Dashboard extends React.Component{
+
+    render(){
+        return(
+            <div>
+                UNDER CONSTRUCTION
+            </div>
+        );
+    }
+
+}
+
+export default Dashboard;
