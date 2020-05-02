ddd152860e58d0e8ce60dbdc147655d7b5a43bf7
Your Name <you@example.com>
2020-05-02 01:47:38 +0100

    Update Feature for the tutors Account

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ class Footer extends React.Component{
             <div>
                 <img  className="container-fluid mb-5" alt="imadge" src="https://res.cloudinary.com/undercover/image/upload/v1586191776/VGG-Udemy_clone/vgg_pww6ap.jpg" width="auto" />
                 <footer className="container-fluid">
-               <div className="row">
+               <div className="row center">
                <div className="col-3 pointer">
                     <p className="link dim">Udemy for Business</p> 
                     <p className="link dim">Teach on Udemy</p>
@@ -27,7 +27,7 @@ class Footer extends React.Component{
                 <p className="link dim">Sitemap</p>
                 <p className="link dim">Featured courses</p>
                 </div>
-                <div className="col-sm-3 ">
+                <div className="col-sm">
                    <img alt="logo" src="https://res.cloudinary.com/undercover/image/upload/v1586194287/VGG-Udemy_clone/logo_xiojdz.png" width="50px"/><span className="f5 dark-green">VENTURE GARDEN GROUP</span>
                    <p>Venture Garden Group is a holding company for a group of financial technology entities dedicated to the innovative and data-driven financial technology solutions.
                         Transforming Africa through technology</p>
--- src/components/fileinput.js
+++ src/components/fileinput.js
@@ -1,53 +1,139 @@
 import React from "react";
-import {Form,Button} from 'react-bootstrap'
+import { Form} from "react-bootstrap";
+import axios from "axios";
+import util from '../util/util'
+import uuid from 'react-uuid'
 class FileInput extends React.Component {
-  // constructor(props) {
-  //   super(props);
-    
-    // this.state = {value: ''};
-    // this.handleChange = this.handleChange.bind(this);
-    // this.handleSubmit = this.handleSubmit.bind(this);
-    // this.fileInput = React.createRef();
-  // }
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      description: '',
+      uploading: false,
+      invalidData:true
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.uploadFile = this.uploadFile.bind(this);
+  }
 
   handleChange(event) {
-    this.setState({value: event.target.value});
+    let nam = event.target.name;
+    this.setState({
+      [nam]: event.target.value
+    })
   }
 
-  handleSubmit(event) {
-    event.preventDefault();
-    alert(
-      `Thank You`
-    );
-  }
+  async uploadFile(e) {
+    e.preventDefault();
+    let nam = e.target.value;
+    if (nam === '') {
+      alert(" input require field");
+    }
+      const uploadPreset = "h13wbqtf";
+      const url = "https://api.cloudinary.com/v1_1/undercover/upload";
+      let formData = new FormData();
+      let file = this.imageUpload.files[0];
+      // console.log(file.name); 
+      formData.append("file", file);
+      formData.append("upload_preset", uploadPreset);
+      formData.append("tags", `uploaded by ${window.localStorage.getItem("name")}`);
+      console.dir(formData);
+      this.setState({ uploading: true });
+      try {
+        var res = await axios.post(url, formData);
+        console.log("Successfully uploaded image", res);
+        var imageUrl;
+        var videoUrl;
+        if (res.data !== undefined && res.data.url !== undefined) {
+          console.log("About uploading video");
+
+          imageUrl = res.data.url;
+          formData = new FormData();
+          let videoFile = this.videoUpload.files[0];
+          formData.append("file", videoFile);
+          formData.append("upload_preset", uploadPreset);
+          formData.append("tags", `uploaded by ${window.localStorage.getItem("name")}`);
+          res = await axios.post(url, formData);
+          console.log("Successfully uploaded video", res);
+
+          if (res.data !== undefined && res.data.url !== undefined) {
+            console.log("About uploading course");
+
+            videoUrl = res.data.url;
+            let uuidString = uuid();
+            console.log(uuidString)
+            let course = {
+              "name": this.state.name,
+              "image": imageUrl,
+              "description": this.state.description,
+              "links": [
+                {
+                  "url": videoUrl,
+                  "title": this.state.id
+                }
+              ]
+            };
+            axios.post(`${util.API_BASE_URL}courses`, course, { headers: { 'content-type': 'application/json' } })
+              .then(res => {
+                console.log("Response is ", res);
+              }).catch(err => {
+                console.error("Error occurred while uploading course to json server due to ", err)
+              })
+
+          }
+        }
+
+
+      } catch (err) {
+        alert("Error occurred during file upload");
+      }
+      
+    
+    }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <Form.Group controlId="exampleForm.ControlInput1">
+      <Form onSubmit={this.uploadFile}>
+        <Form.Group controlId="Form.ControlInput1">
           <Form.Label>Course Name</Form.Label>
-          <Form.Control type="text" placeholder="Course" />
+          <Form.Control onChange={this.handleChange} name="name" type="text" placeholder="Course" />
         </Form.Group>
-        <Form.Group controlId="exampleForm.ControlInput1">
+        <Form.Group controlId="Form.ControlInput2">
           <Form.Label>Course Title </Form.Label>
-          <Form.Control type="text" placeholder="course title" />
+          <Form.Control onChange={this.handleChange} name="id" type="text" placeholder="course title" />
         </Form.Group>
-        <Form.Group controlId="exampleForm.ControlTextarea1">
-          <Form.Label>Course Decsription</Form.Label>
-          <Form.Control as="textarea" rows="3" />
+        <Form.Group controlId="Form.ControlTextarea1">
+          <Form.Label>Course Description</Form.Label>
+          <Form.Control onChange={this.handleChange} name="description" as="textarea" rows="3" />
         </Form.Group>
-        <Form.Group controlId="exampleForm.ControlTextarea1">
-          <Form.File
-            id="File"
-            label="Upload File"
+        <Form.Group controlId="Form.ControlFile1">
+        <Form.Label>Choose Video to Upload </Form.Label>
+        
+          <input type="file" name="file" label= "Choose Video to upload"
+            id="videoFile"
+            ref={
+              file => (this.videoUpload = file)
+            }
             custom
-            
           />
         </Form.Group>
-        <Button onClick={()=>this.handleSubmit} variant="primary" type="submit">
-          Upload 
-        </Button>
-      </form>
+        <Form.Label>Choose Image to Upload </Form.Label>
+          <input type="file" name="file"
+            label= "Choose Image to upload"
+            id="imageFile"
+            ref={
+              file => (this.imageUpload = file)
+            }
+            custom
+          />
+          <p id="filename"> </p>
+        
+        <input type='submit' variant="primary" className='btn btn-primary btn-sm' 
+        value={!this.state.uploading ? 'Update  Course Content ' : 'Upload in progress'} 
+        disabled={this.state.uploading && !(this.state.description)} />
+        
+
+      </Form>
     );
   }
 }
