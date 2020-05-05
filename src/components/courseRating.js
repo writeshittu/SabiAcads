5fa7e39cde86a14fc85994d600c565af59c340f7
shit2boy <writeshittu@gmail.com>
2020-05-05 13:17:43 +0100

    new features

--- src/components/courseRating.js
+++ src/components/courseRating.js
@@ -1,45 +1,6 @@
 import  React,{useState} from "react";
 import {FaStar} from 'react-icons/fa';
 
-// import {StarRatings} from 'react-star-ratings';
- 
-// class VideoRating extends React.Component {
-//     changeRating( newRating, name ) {
-//       this.setState({
-//         rating: newRating
-//       });
-//     }
- 
-//     render() {
-//       // rating = 2;
-//       return (
-//         <StarRatings
-//           rating={this.state.rating}
-//           starRatedColor="golden"
-//           changeRating={this.changeRating}
-//           numberOfStars={5}
-//           name='rating'
-//         />
-//       );
-//     }
-// }
- 
- 
-// class StarBar extends React.Component {
-//   render() {
-//     // aggregateRating = 2.35;
-//     return (
-//       <StarRatings
-//         rating={2.403}
-//         starDimension="20px"
-//         starSpacing="3px"
-//       />
-//     );
-//   }
-// }
-// export default StarBar + ;
-
-
 const StarRating =()=>{
   const[ rating, setRating] = useState('');
   const[ hover, setHover] = useState('');
@@ -48,7 +9,7 @@ const StarRating =()=>{
       {[...Array(6)].map((star, i) => { 
         const ratingValue = i++ ;
       return(
-        <label>
+        <label key={Array.index} >
           <input type="radio" style ={{display:"none"}} name="rating" value ="ratingValue" 
           onClick = {()=>setRating(ratingValue)} 
           
